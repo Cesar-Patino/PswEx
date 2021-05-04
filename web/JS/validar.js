88fb22b9b96c5d266c06e427d253172dbeb4e77d
Dante1315 <80567599+Dante1315@users.noreply.github.com>
2021-05-03 19:28:52 -0500

    js

---
+++ web/JS/validar.js
@@ -0,0 +1,40 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function validare(e) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valor)){
+     alert("La dirección de email " + valor + " es correcta.");
+    } else {
+     alert("La dirección de email es incorrecta.");
+    }
+  }
+
+
+
+//Validar solo numeros
+function validarn(e){
+    var teclado = (document.all)?e.keyCode:e.which;
+    if(teclado == 8)return true;
+
+    var patron = /[0-9\d .]/;
+
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
+//Validar solo palabras y espcacions
+
+function validarp(e){
+    var teclado = (document.all)?e.keyCode:e.which;
+    if(teclado == 8)return true;
+
+    var patron = /^[a-zA-Z ]*$/;
+
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
